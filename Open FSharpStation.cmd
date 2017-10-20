78fa1f2994335024ec771786e1e90eae32c1049f
amieres <amieres@hotmail.com>
2017-10-20 07:31:47 -0500

    Update .cmd

--- Open FSharpStation.cmd
+++ Open FSharpStation.cmd
@@ -2,7 +2,7 @@ rem paket.exe init
 rem paket.exe install
 rem paket.exe add FSharp.Compiler.Tools --version 4.1.23
 
-start /b chrome --enable-blink-features=ResizeObserver "localhost:9010"
+start /b chrome "http://localhost:9010/" --enable-blink-features=ResizeObserver 
 start /b FsAutoComplete\FsAutoComplete.exe --mode http --port 9001
 cd bin
 set path=%path%;..\packages\FSharp.Compiler.Tools\tools;..\packages\Zafir.FSharp\tools;.
