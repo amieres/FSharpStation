5b8922fb6875b37b74ec978fb204d97450dbca05
amieres <amieres@hotmail.com>
2018-10-17 03:08:19 -0500

    gitignore

--- Open FSharpStation.cmd
+++ Open FSharpStation.cmd
@@ -6,6 +6,7 @@ rem paket.exe install
 rem paket.exe add FSharp.Compiler.Tools --version 4.1.23
 
 rem start /b chrome --enable-blink-features=ResizeObserver "http://localhost:9005/" 
+set path=%path%;"D:\Abe\CIPHERWorkspace\FSharpStation\packages\Test\FSharp.Core\lib\net45
 start /b FsAutoComplete\FsAutoComplete.exe --mode http --port 9001
 rem cd bin
 set path=%path%;%CD%\..\..\FSharpStation\packages\FSharp.Compiler.Tools\tools;%CD%\..\..\FSharpStation\WebSharperCompiler
