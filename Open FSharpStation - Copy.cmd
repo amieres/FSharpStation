e7ddbf5d81343dd9d7d433176e3c3a4db52285fb
amieres <amieres@hotmail.com>
2020-06-14 13:09:31 -0500

    several

--- Open FSharpStation - Copy.cmd
+++ Open FSharpStation - Copy.cmd
@@ -7,7 +7,7 @@ rem paket.exe add FSharp.Compiler.Tools --version 4.1.23
 
 rem start /b chrome --enable-blink-features=ResizeObserver "http://localhost:9005/" 
 set path=%path%;"D:\Abe\CIPHERWorkspace\FSharpStation\packages\Test\FSharp.Core\lib\net45
-start /b D:\Abe\CIPHERWorkspace\Repos\FsAutoComplete-master\src\FsAutoComplete\bin\Debug\net461\FsAutoComplete.exe --mode http --port 9001
+start /b D:\Abe\CIPHERWorkspace\Repos\FsAutoComplete\src\FsAutoComplete\bin\Debug\net461\FsAutoComplete.exe --mode http --port 9001
 cd bin
 set path=%path%;%CD%\..\packages\FSharp.Compiler.Tools\tools;%CD%\..\packages\WebSharper.FSharp\tools\net461
 start FSharpStation.exe
