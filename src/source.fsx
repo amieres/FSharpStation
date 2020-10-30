888152fd0c88a4b3dc2dd556781ae655931e1368
amieres <amieres@hotmail.com>
2020-10-30 05:29:02 -0500

    Some changes

--- src/source.fsx
+++ src/source.fsx
@@ -1,6 +1,13 @@
+#r @"D:\Abe\CIPHERWorkspace\FSharpStation\packages\other\Lego.Mindstorms\lib\netstandard2.0\Lego.Mindstorms.dll"
 
-        type Hello  = Hello
-        type Hello2 = Hello2
-        let add  (a:Hello ) = printfn "%A" a
-        let add2 (a:Hello2) = printfn "%A" a
-        
+open Lego.Mindstorms
+
+let turn() = async {
+    use communication = new UsbCommunication()
+    use client = new Lego.Mindstorms.MindstormsClient<UsbCommunication>(communication)
+    do! client.ConnectAsync()                                               |> Async.AwaitTask
+    do! client.TurnMotorAtPowerForTimeAsync(OutputPort.A, 50, 3000u, false) |> Async.AwaitTask
+    do! client.PlayToneAsync(50, 5000us, 500us)                             |> Async.AwaitTask
+}
+
+turn() |> Async.RunSynchronously
